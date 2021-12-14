224f9abb7466a2c694abf5d7150f7b9054b1c5cc
BohdanSizomin <nbzp28@gmail.com>
2021-12-14 19:27:07 +0100

    Checkout page added (cart included) Assigning user to Items in Cart Handled variation between anonymous user cart and logged in user cart Fixed cart increment and decrement function Dynamically redirection of anonymous user from 'cart->login' page to 'checkout/' page

--- users/views.py
+++ users/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from .models import *
+from cart.models import Cart, CartItem
 from .forms import SigupForm
+from cart.views import _cart_id
 # Verifiation email
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes
@@ -11,6 +13,7 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
+import requests
 
 
 # Create your views here.
@@ -22,9 +25,60 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
 
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+
+                    # Getting product variation by cart id
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+
+                    # Getting the cart items from user to access his product variation
+
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item.id = id[index]
+                            item = CartItem.objects.get(id=item.id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+
+
+            except:
+                pass
             auth.login(request, user)
             messages.success(request, 'You have logged in successfully')
-            return redirect('index')
+
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+
+                # next=/cart/checkout
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('profile')
+
         else:
             messages.error(request, 'Invalid login credentials')
             return redirect('login')
