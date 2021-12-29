d90e13925d53fa02f4d81e5eff3e7aee194cef47
BohdanSizomin <nbzp28@gmail.com>
2021-12-29 15:40:01 +0100

    commit

--- Django_SportNutrition_Ecommerce/settings.py
+++ Django_SportNutrition_Ecommerce/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-t8+r&^h_2(5==(@h@pb=s8sbxlkhr2h7x)43q4#gx)aqr-)-7f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -149,8 +149,8 @@ MESSAGE_TEXT = {
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'nbzp28@gmail.com'
+EMAIL_HOST_PASSWORD = 'odxqktveylvvpzlf'
 EMAIL_USE_TLS = True
 
 # Log out after 1hour of inactivity
