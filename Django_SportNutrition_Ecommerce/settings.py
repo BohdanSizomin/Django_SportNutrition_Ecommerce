293af54c89dbc68c02a2c9b387623b66e285211e
BohdanSizomin <nbzp28@gmail.com>
2022-01-04 13:08:56 +0100

    debug=true

--- Django_SportNutrition_Ecommerce/settings.py
+++ Django_SportNutrition_Ecommerce/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://django-sportnutrition-ecom.herokuapp.com/']
+ALLOWED_HOSTS = ['https://django-sportnutrition-ecom.herokuapp.com/',]
 
 # Application definition
 
